9f20057c6cd9a52cab48e8eb6d3b29f98023b330
frankdb <fd.barros@yahoo.com>
2019-03-26 21:11:44 -0700

    Added calculate function

--- src/app.jsx
+++ src/app.jsx
@@ -4,9 +4,17 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      amountDue: '',
-      amountReceived: '',
-      changeDue: null
+      amountDue: 0,
+      amountReceived: 0,
+      changeDue: null,
+      twenties: null,
+      tens: null,
+      fives: null,
+      ones: null,
+      quarters: null,
+      dimes: null,
+      nickels: null,
+      pennies: null
     }
     this.handleChange = this.handleChange.bind(this);
     this.calculate = this.calculate.bind(this);
@@ -14,15 +22,38 @@ class App extends Component {
 
   handleChange(e) {
     if (e.target.name === 'amountDue') {
-      this.setState({ amountDue: Number(e.target.value) });
+      this.setState({ amountDue: e.target.value });
     } else if (e.target.name === 'amountReceived') {
-      this.setState({ amountReceived: Number(e.target.value) });
+      this.setState({ amountReceived: e.target.value });
     }
   }
 
   calculate(amountDue, amountReceived) {
-    const changeDue = amountReceived - amountDue;
-    this.setState({ changeDue: changeDue });
+    let changeDue = amountReceived - amountDue;
+    this.setState({ changeDue: changeDue.toFixed(2) });
+
+    if (this.state.changeDue < 0) {
+      return;
+    }
+
+    const coinTypes = ["twenties", "tens", "fives", "ones", "quarters", "dimes", "nickels", "pennies"];
+    const coinValues = [20, 10, 5, 1, .25, .1, .05, .01];
+
+    var amount;
+
+    for (var i = 0; i < coinValues.length; i++) {
+      amount = Math.floor((changeDue) / coinValues[i]);
+      if (amount > 0) {
+        let coin = coinTypes[i];
+        this.setState({ [coin]: amount })
+        // $(`#${coinTypes[i]}-output`).append(`${amount}`);
+        changeDue = changeDue % coinValues[i];
+      } else {
+        let coin = coinTypes[i];
+        this.setState({ [coin]: amount })
+        // $(`#${coinTypes[i]}-output`).append("0");
+      }
+    }
   }
 
   render() {
@@ -62,14 +93,23 @@ class App extends Component {
 
             <div className="card" style={{ padding: '20px' }}>
 
-              {this.state.changeDue ? this.state.changeDue > 0 ? (<div className="alert alert-success text-center"> The total change due is: ${this.state.changeDue}</div>) : (<div className="alert alert-danger text-center"> Additional money owed </div>) : ''}
+              {this.state.changeDue ? this.state.changeDue > 0 ? (<div className="alert alert-success text-center"> The total change due is ${this.state.changeDue}</div>) : (<div className="alert alert-danger text-center"> Additional money owed </div>) : ''}
 
               <div className="row" style={{ paddingBottom: '20px' }}>
                 <div className="col-md-3">
                   <div className="card" style={{ backgroundColor: '#F5F5F5' }}>
                     <div className="card-body">
-                      <h5 className="card-title">Twenties</h5>
-                      <p>0</p>
+                      <h5 className="card-title text-center">Twenties</h5>
+                      <p className="text-center change">{this.state.twenties}</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div classNameName="col-md-3">
+                  <div className="card" style={{ backgroundColor: '#F5F5F5' }}>
+                    <div className="card-body">
+                      <h5 className="card-title text-center">Tens</h5>
+                      <p className="text-center change">{this.state.tens}</p>
                     </div>
                   </div>
                 </div>
@@ -77,8 +117,8 @@ class App extends Component {
                 <div className="col-md-3">
                   <div className="card" style={{ backgroundColor: '#F5F5F5' }}>
                     <div className="card-body">
-                      <h5 className="card-title">Tens</h5>
-                      <p>0</p>
+                      <h5 className="card-title text-center">Fives</h5>
+                      <p className="text-center change">{this.state.fives}</p>
                     </div>
                   </div>
                 </div>
@@ -86,17 +126,8 @@ class App extends Component {
                 <div className="col-md-3">
                   <div className="card" style={{ backgroundColor: '#F5F5F5' }}>
                     <div className="card-body">
-                      <h5 className="card-title">Fives</h5>
-                      <p>0</p>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col-md-3">
-                  <div className="card" style={{ backgroundColor: '#F5F5F5' }}>
-                    <div className="card-body">
-                      <h5 className="card-title">Ones</h5>
-                      <p>0</p>
+                      <h5 className="card-title text-center">Ones</h5>
+                      <p className="text-center change">{this.state.ones}</p>
                     </div>
                   </div>
                 </div>
@@ -107,8 +138,8 @@ class App extends Component {
                 <div className="col-md-3">
                   <div className="card" style={{ backgroundColor: '#F5F5F5' }}>
                     <div className="card-body">
-                      <h5 className="card-title">Quarters</h5>
-                      <p>0</p>
+                      <h5 className="card-title text-center">Quarters</h5>
+                      <p className="text-center change">{this.state.quarters}</p>
                     </div>
                   </div>
                 </div>
@@ -116,8 +147,8 @@ class App extends Component {
                 <div className="col-md-3">
                   <div className="card" style={{ backgroundColor: '#F5F5F5' }}>
                     <div className="card-body">
-                      <h5 className="card-title">Dimes</h5>
-                      <p>0</p>
+                      <h5 className="card-title text-center">Dimes</h5>
+                      <p className="text-center change">{this.state.dimes}</p>
                     </div>
                   </div>
                 </div>
@@ -125,8 +156,8 @@ class App extends Component {
                 <div className="col-md-3">
                   <div className="card" style={{ backgroundColor: '#F5F5F5' }}>
                     <div className="card-body">
-                      <h5 className="card-title">Nickels</h5>
-                      <p>0</p>
+                      <h5 className="card-title text-center">Nickels</h5>
+                      <p className="text-center change">{this.state.nickels}</p>
                     </div>
                   </div>
                 </div>
@@ -134,8 +165,8 @@ class App extends Component {
                 <div className="col-md-3">
                   <div className="card" style={{ backgroundColor: '#F5F5F5' }}>
                     <div className="card-body">
-                      <h5 className="card-title">Pennies</h5>
-                      <p>0</p>
+                      <h5 className="card-title text-center">Pennies</h5>
+                      <p className="text-center change">{this.state.pennies}</p>
                     </div>
                   </div>
                 </div>
